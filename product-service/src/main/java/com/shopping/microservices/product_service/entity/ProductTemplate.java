8f5e5312dedf01fd842ee41cb5677ffedaf170fb
tinhonline262 <tinhonline262@gmail.com>
2026-01-20 11:32:27 +0700

    add entity for product attribute, and flyway migration

---
+++ product-service/src/main/java/com/shopping/microservices/product_service/entity/ProductTemplate.java
@@ -0,0 +1,43 @@
+package com.shopping.microservices.product_service.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "product_template", schema = "product_service_db")
+public class ProductTemplate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 255)
+    @NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @ColumnDefault("CURRENT_TIMESTAMP")
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Size(max = 255)
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Size(max = 255)
+    @Column(name = "last_modified_by")
+    private String lastModifiedBy;
+
+}
